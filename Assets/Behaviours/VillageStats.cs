dace9b0b23af92bf53047c201ab32572a7e1eb14
elliottsmith1 <elliottsmith1@hotmail.co.uk>
2017-11-22 14:31:27 +0000

    Simple script to hold village variables

---
+++ Assets/Behaviours/VillageStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageStats : MonoBehaviour {
+
+    private float village_money = 150;  //currency
+    private float village_population = 500; //villagers
+    private float village_happiness = 30;   //approval rating 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float GetMoney()
+    {
+        return village_money;
+    }
+
+    public void SetMoney(float money)
+    {
+        village_money += money;
+    }
+
+    public float GetPopulation()
+    {
+        return village_population;
+    }
+
+    public void SetPopulation(float population)
+    {
+        village_population += population;
+    }
+
+    public float GetHappiness()
+    {
+        return village_happiness;
+    }
+
+    public void SetHappiness(float happiness)
+    {
+        village_happiness += happiness;
+    }
+}
