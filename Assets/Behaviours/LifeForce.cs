a077da62bec1352259a92cd3dbe8d9e1a351ad7c
zkajota <zkajota@gmail.com>
2017-11-23 12:55:29 +0000

    Merge branch 'master' of https://github.com/Jdasi/CGD_Game_Jam_3
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ Assets/Behaviours/LifeForce.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LifeForce : MonoBehaviour
+{
+    [SerializeField] private int max_health = 100;
+
+    public CustomEvents.GameObjectEvent on_death_event;
+    public CustomEvents.IntEvent on_damage_event;
+
+    private int current_health = 100;
+
+
+    void Awake()
+    {
+        CreateEvents();
+    }
+
+
+    private void CreateEvents()
+    {
+        if (on_death_event == null)
+            on_death_event = new CustomEvents.GameObjectEvent();//create event
+
+        if (on_damage_event == null)
+            on_damage_event = new CustomEvents.IntEvent();
+    }
+
+
+    public bool Damage(int _damage)
+    {
+        if (current_health <= 0)
+            return false;
+
+        current_health -= _damage;//damage health
+
+        if (current_health > 0)
+        {
+            on_damage_event.Invoke(_damage);//trigger damage event if survived
+            return false;
+        }
+
+        current_health = 0;
+        on_death_event.Invoke(gameObject);//trigger death event
+
+        return true;
+    }
+
+
+    public void Heal(int _heal_amount)
+    {
+        current_health += _heal_amount;
+        current_health = Mathf.Clamp(current_health, 0, max_health);//clamp to max value
+    }
+
+
+    public void ResetHealth()
+    {
+        current_health = max_health;
+    }
+
+
+    public void SetMaxHealth(int _max_health, bool _update_current_health = true)
+    {
+        max_health = _max_health;
+
+        if (!_update_current_health)
+            return;
+
+        ResetHealth();//update current health if specified
+    }
+
+}
