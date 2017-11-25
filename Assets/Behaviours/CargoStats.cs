c49349e57e1c704540d6939614be112737598f35
elliottsmith1 <elliottsmith1@hotmail.co.uk>
2017-11-25 12:55:28 +0000

    drop off zone prefabbed

---
+++ Assets/Behaviours/CargoStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoStats : MonoBehaviour {
+
+    [SerializeField] float cargo_value = 10;
+
+	// Use this for initialization
+	void Start () {
+        cargo_value = Random.Range(5, 25);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float GetValue()
+    {
+        return cargo_value;
+    }
+}
--- Assets/Behaviours/LootDropPoint.cs
+++ Assets/Behaviours/LootDropPoint.cs
@@ -6,13 +6,24 @@ public class LootDropPoint : MonoBehaviour
 {
     [SerializeField] string tag;
     [SerializeField] float despawn_timer;
+    private VillageStats stats;
+
+    void Start()
+    {
+        stats = GetComponent<VillageStats>();
+    }
 
     // Detect Loot Dropped
     void OnTriggerEnter(Collider collider)
-    {
+    { 
         if (collider.CompareTag(tag))
         {
+            float current_money = stats.GetMoney();
+
+            float cargo_value = collider.GetComponent<CargoStats>().GetValue();
+
             // increase player score ect.
+            stats.SetMoney(current_money + cargo_value);
 
             // despawn loot container.
             Destroy(collider.gameObject, despawn_timer);
