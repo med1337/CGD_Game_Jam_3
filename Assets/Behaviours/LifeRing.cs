4ac8b311bb3f87da8b4bf0e2a8cbe28bd9cb73c2
Jdasi <silvacore@hotmail.co.uk>
2017-11-27 15:27:10 +0000

    added life ring station

---
+++ Assets/Behaviours/LifeRing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeRing : MonoBehaviour
+{
+    [Header("Parameters")]
+    public Transform trajectory_end_ref;
+
+    [Header("References")]
+    public Rigidbody rigid_body;
+
+}
--- Assets/Behaviours/LifeRingControl.cs
+++ Assets/Behaviours/LifeRingControl.cs
@@ -14,22 +14,32 @@ public class LifeRingControl : Controllable
     [SerializeField] float reticule_max_distance;
 
     [Space]
-    [SerializeField] float shot_delay;
-    [SerializeField] float shot_spread;
-    [SerializeField] GameObject shot_prefab;
+    [SerializeField] float shoot_strength;
+    [SerializeField] CurvedLineRenderer rope_main;
+    [SerializeField] Rigidbody rope_mid;
+    [SerializeField] Transform rope_end;
 
     [Space]
-    [SerializeField] GameObject particle_prefab;
+    [SerializeField] GameObject life_ring_prefab;
+    [SerializeField] float max_ring_distance;
 
     [Header("References")]
     [SerializeField] GameObject turret;
     [SerializeField] Transform shoot_point;
     [SerializeField] Transform ejection_point;
+    [SerializeField] Transform trajectory_end;
 
     private Vector3 reticule_pos;
     private Vector3 reticule_offset;
     private GameObject reticule_vis;
     private Vector3 target_forward;
+
+    Vector3 left_max;
+    Vector3 right_max;
+
+    private bool life_ring_out { get { return life_ring != null; } }
+    private bool can_activate = true;
+    private LifeRing life_ring;
 
 
     public override void Move(Vector3 _dir)
@@ -58,33 +68,56 @@ public class LifeRingControl : Controllable
     }
 
 
+    // Called as long as the controlling player holds the Attack button.
     public override void Activate()
     {
-        
+        if (!life_ring_out && can_activate)
+        {
+            can_activate = false;
+
+            ThrowRing();
+        }
+        else if (life_ring_out && can_activate)
+        {
+            can_activate = false;
+
+            RetractRing();
+        }
     }
 
 
+    // Called when the controlling player releases the Attack button.
     public override void Stop()
     {
-
+        can_activate = true;
     }
 
 
-    void Shoot()
+    void ThrowRing()
     {
         Vector3 shot_forward = shoot_point.forward;
-        Vector3 variance = new Vector3(
-            Random.Range(-shot_spread, shot_spread),
-            Random.Range(-shot_spread, shot_spread),
-            Random.Range(-shot_spread, shot_spread));
-        shot_forward += variance;
 
-        GameObject shot_clone = Instantiate(shot_prefab, shoot_point.position,
-            Quaternion.LookRotation(shot_forward));
-        TurretShot shot = shot_clone.GetComponent<TurretShot>();
+        GameObject clone = Instantiate(life_ring_prefab, shoot_point.position + shot_forward, Quaternion.identity);
+        life_ring = clone.GetComponent<LifeRing>();
+        life_ring.rigid_body.AddForce(shot_forward * (shoot_strength * move_dir.magnitude), ForceMode.Impulse);
+        life_ring.trajectory_end_ref = trajectory_end;
 
-        GameObject particle_clone = Instantiate(particle_prefab, shoot_point.position,
-            Quaternion.LookRotation(shot_forward));
+        rope_mid.gameObject.SetActive(true);
+        rope_end.gameObject.SetActive(true);
+
+        rope_mid.transform.position = shoot_point.position;
+        rope_mid.AddForce(shot_forward * (shoot_strength / 2), ForceMode.Impulse);
+    }
+
+
+    void RetractRing()
+    {
+        Destroy(life_ring.gameObject);
+
+        rope_mid.gameObject.SetActive(false);
+        rope_end.gameObject.SetActive(false);
+
+        rope_main.UpdatePoints();
     }
 
 
@@ -96,30 +129,84 @@ public class LifeRingControl : Controllable
 
     void Update()
     {
+        left_max = Quaternion.Euler(0, -max_steer_angle, 0) * transform.forward;
+        right_max = Quaternion.Euler(0, max_steer_angle, 0) * transform.forward;
+
+        if (life_ring != null)
+        {
+            rope_end.position = life_ring.transform.position;
+            rope_main.UpdatePoints();
+        }
+
         if (being_controlled)
             ControlUpdate();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (life_ring == null)
+            return;
+
+        // Keep the life ring in range.
+        if (Vector3.Distance(life_ring.transform.position, transform.position) > max_ring_distance)
+        {
+            Vector3 diff = (transform.position - life_ring.transform.position);
+            life_ring.rigid_body.MovePosition(life_ring.transform.position + diff * Time.fixedDeltaTime);
+        }
+
+        // Keep the mid rope point in range.
+        if (Vector3.Distance(rope_mid.transform.position, transform.position) > max_ring_distance / 2)
+        {
+            Vector3 diff = (transform.position - rope_mid.transform.position);
+            rope_mid.MovePosition(rope_mid.transform.position + diff * Time.fixedDeltaTime);
+        }
     }
 
 
     void ControlUpdate()
     {
         reticule_offset = move_dir * reticule_max_distance;
-        reticule_pos = turret.transform.position + reticule_offset;
-        reticule_pos += turret.transform.forward * reticule_min_distance;
+        reticule_pos = transform.position + reticule_offset;
+        reticule_pos += transform.forward * reticule_min_distance;
 
         if (reticule_vis != null)
             reticule_vis.transform.position = reticule_pos;
 
-        Vector3 new_forward = (reticule_pos - turret.transform.position).normalized;
+        Vector3 new_forward = (reticule_pos - transform.position).normalized;
         float angle = Vector3.Angle(transform.forward, new_forward);
 
         if (angle <= max_steer_angle)
         {
             target_forward = new_forward;
         }
+        else
+        {
+            float l_angle = Vector3.Angle(new_forward, left_max);
+            float r_angle = Vector3.Angle(new_forward, right_max);
+
+            if (l_angle > r_angle)
+            {
+                target_forward = right_max;
+            }
+            else
+            {
+                target_forward = left_max;
+            }
+        }
 
         Vector3 slerp = Vector3.Slerp(turret.transform.forward, target_forward, steer_speed * Time.deltaTime);
         turret.transform.forward = slerp;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, left_max * 10);
+        Gizmos.DrawRay(transform.position, right_max * 10);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, target_forward * 10);
     }
 
 }
