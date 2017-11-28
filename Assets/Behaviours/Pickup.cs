3971fece4ee9bf6a423ce1a56140759cfc84c78d
Jdasi <silvacore@hotmail.co.uk>
2017-11-28 22:18:39 +0000

    removed pickup cleanup (rip refuges)

--- Assets/Behaviours/Pickup.cs
+++ Assets/Behaviours/Pickup.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    [SerializeField] float cleanup_after = 60;
-
     public bool outline_enabled
     {
         get
@@ -28,13 +26,10 @@ public class Pickup : MonoBehaviour
     private Transform trajectory_end;
     private float hook_progress;
 
-    private float cleanup_timer;
-
 
     public void SetInUse(bool _in_use)
     {
         in_use = _in_use;
-        cleanup_timer = 0;
     }
 
 
@@ -46,22 +41,6 @@ public class Pickup : MonoBehaviour
 
     void Update()
     {
-        if (GetInUse())
-        {
-            cleanup_timer = 0;
-        }
-        else
-        {
-            float prev_timer = cleanup_timer;
-            cleanup_timer += Time.deltaTime;
-
-            if (prev_timer < cleanup_after && cleanup_timer >= cleanup_after)
-            {
-                Destroy(GetComponent<Collider>());
-                Destroy(this.gameObject, 5);
-            }
-        }
-
         if (hooked)
             HookedUpdate();
     }
@@ -89,7 +68,6 @@ public class Pickup : MonoBehaviour
     {
         if (_other.CompareTag("Deck"))
         {
-            cleanup_timer = 0;
             transform.SetParent(_other.transform);
         }
     }
