2b0cc83a661d1b3820a8b08ffa307a6534282e3a
zkajota <zbigniew2.kajota@live.uwe.ac.uk>
2017-12-14 15:01:34 +0000

    Player score prefab and script.

---
+++ Assets/Behaviours/PlayerSCORESscript.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PlayerSCORESscript : MonoBehaviour
+{
+    //player gameobjects
+    
+    [Header("Parameters")]
+    int p1Score = 0; int p2Score = 0;
+    int p3Score = 0; int p4Score = 0;
+
+    [Header("References")]
+    [SerializeField] Text p1Display;
+    [SerializeField] Text p2Display;
+    [SerializeField] Text p3Display;
+    [SerializeField] Text p4Display;
+
+    private int playerID;
+
+    public void IncreasePlayerScore(int player, int score)
+    {
+        if (player == 1)
+        {
+            p1Score+= score;
+            p1Display.text = p1Score.ToString();
+        }
+        if (player == 2)
+        {
+            p2Score+= score;
+            p2Display.text = p2Score.ToString();
+        }
+        if (player == 3)
+        {
+            p3Score+= score;
+            p3Display.text = p3Score.ToString();
+        }
+        if (player == 4)
+        {
+            p4Score+= score;
+            p4Display.text = p4Score.ToString();
+        }
+    }
+
+    void Update()
+    {
+
+    }
+    //[Header("Parameters")]
+    //[SerializeField] int starting_cash = 1000;
+
+    //[Space]
+    //[SerializeField] float collection_delay = 60;
+    //[SerializeField] int collection_amount = 500;
+
+    //[Space]
+    //[SerializeField] UnityEvent score_gained_events;
+    //[SerializeField] UnityEvent bankrupt_events;
+
+    //[Header("References")]
+    //[SerializeField] Text cash_display;
+    //[SerializeField] Text time_display;
+    //[SerializeField] Text collection_amount_display;
+    //[SerializeField] Text next_collection_display;
+
+    //private int current_cash;
+    //public int accumulated_cash { get; private set; }
+
+    //public float play_time { get; private set; }
+    //private float collection_countdown;
+
+
+    //public void InsertDoubloon()
+    //{
+    //    current_cash = starting_cash;
+    //    collection_countdown = collection_delay;
+
+    //    cash_display.text = current_cash.ToString();
+    //    collection_amount_display.text = "$" + collection_amount.ToString();
+    //}
+
+
+    //public string GetTimePlayedString()
+    //{
+    //    return time_display.text;
+    //}
+
+
+    //public void IncreaseScore(int _amount)
+    //{
+    //    current_cash += _amount;
+    //    accumulated_cash += _amount;
+
+    //    cash_display.text = current_cash.ToString();
+
+    //    score_gained_events.Invoke();
+    //}
+
+
+    void Start()
+    {
+        p1Score = 0;
+        p2Score = 0;
+        p3Score = 0;
+        p4Score = 0;
+    }
+
+
+    //void Update()
+    //{
+    //    play_time += Time.deltaTime;
+    //    collection_countdown -= Time.deltaTime;
+
+    //    UpdateTimerDisplay(play_time, time_display);
+    //    UpdateTimerDisplay(collection_countdown, next_collection_display);
+
+    //    if (collection_countdown <= 0)
+    //    {
+    //        collection_countdown = collection_delay;
+    //        collection_amount_display.text = "$" + collection_amount.ToString(); // in case we change the collection amount.
+
+    //        CollectPayment();
+    //    }
+    //}
+
+
+    //void UpdateTimerDisplay(float _f, Text _display)
+    //{
+    //    System.TimeSpan t = System.TimeSpan.FromSeconds(_f);
+    //    _display.text = string.Format("{0:00}:{1:00}", t.Minutes, t.Seconds);
+    //}
+
+
+    //void CollectPayment()
+    //{
+    //    current_cash -= collection_amount;
+    //    cash_display.text = current_cash.ToString();
+
+    //    if (current_cash <= 0)
+    //    {
+    //        bankrupt_events.Invoke();
+    //    }
+    //}
+
+}
